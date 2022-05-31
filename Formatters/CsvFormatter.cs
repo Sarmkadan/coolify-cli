0dfc7a3624ebb0039c07d2b891063ac0ed8c8529
Vladyslav Zaiets <rutova2@gmail.com>
2022-05-31 17:11:24 +0300

    Validate StatusCheckWorker args

---
+++ Formatters/CsvFormatter.cs
@@ -0,0 +1,197 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Reflection;
+using System.Text;
+
+namespace CoolifiCli.Formatters;
+
+/// <summary>
+/// Formats data as CSV output. Handles proper escaping, quoting, and delimiter handling.
+/// Supports custom delimiters, header rows, and field selection.
+/// </summary>
+public class CsvFormatter : IOutputFormatter
+{
+    private readonly char _delimiter;
+    private readonly bool _includeHeader;
+    private readonly List<string>? _selectedFields;
+
+    public CsvFormatter(char delimiter = ',', bool includeHeader = true, List<string>? selectedFields = null)
+    {
+        _delimiter = delimiter;
+        _includeHeader = includeHeader;
+        _selectedFields = selectedFields;
+    }
+
+    /// <summary>
+    /// Formats a single object as a CSV line.
+    /// </summary>
+    public string Format(object? data)
+    {
+        if (data == null)
+            return string.Empty;
+
+        var properties = GetProperties(data.GetType());
+        var values = properties.Select(p => FormatValue(p.GetValue(data)));
+
+        return CombineValues(values);
+    }
+
+    /// <summary>
+    /// Formats a collection of objects as CSV with header row.
+    /// </summary>
+    public string FormatCollection<T>(IEnumerable<T> items)
+    {
+        var itemsList = items.ToList();
+        if (itemsList.Count == 0)
+            return string.Empty;
+
+        var sb = new StringBuilder();
+        var properties = GetProperties(typeof(T));
+
+        // Write header row if requested
+        if (_includeHeader)
+        {
+            var headers = properties.Select(p => EscapeCsvField(p.Name));
+            sb.AppendLine(CombineValues(headers));
+        }
+
+        // Write data rows
+        foreach (var item in itemsList)
+        {
+            var values = properties.Select(p => FormatValue(p.GetValue(item)));
+            sb.AppendLine(CombineValues(values));
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    /// <summary>
+    /// Formats a dictionary as a CSV line.
+    /// </summary>
+    public string FormatDictionary(Dictionary<string, object?> data)
+    {
+        if (data == null || data.Count == 0)
+            return string.Empty;
+
+        var sb = new StringBuilder();
+
+        if (_includeHeader)
+        {
+            var headers = data.Keys.Select(k => EscapeCsvField(k));
+            sb.AppendLine(CombineValues(headers));
+        }
+
+        var values = data.Values.Select(FormatValue);
+        sb.Append(CombineValues(values));
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// Converts CSV data to a dictionary with headers as keys.
+    /// </summary>
+    public List<Dictionary<string, string>> ParseCsv(string csvContent)
+    {
+        var lines = csvContent.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+        var result = new List<Dictionary<string, string>>();
+
+        if (lines.Length < 2)
+            return result;
+
+        var headers = lines[0].Split(_delimiter);
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            if (string.IsNullOrWhiteSpace(lines[i]))
+                continue;
+
+            var values = lines[i].Split(_delimiter);
+            var row = new Dictionary<string, string>();
+
+            for (int j = 0; j < headers.Length && j < values.Length; j++)
+            {
+                row[headers[j].Trim()] = UnescapeCsvField(values[j].Trim());
+            }
+
+            result.Add(row);
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Gets properties from an object type, filtering by selected fields if provided.
+    /// </summary>
+    private PropertyInfo[] GetProperties(Type type)
+    {
+        var allProperties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        if (_selectedFields == null || _selectedFields.Count == 0)
+            return allProperties;
+
+        return allProperties
+            .Where(p => _selectedFields.Contains(p.Name, StringComparer.OrdinalIgnoreCase))
+            .ToArray();
+    }
+
+    /// <summary>
+    /// Formats a single value for CSV output with proper escaping.
+    /// </summary>
+    private string FormatValue(object? value)
+    {
+        if (value == null)
+            return string.Empty;
+
+        if (value is DateTime dt)
+            return EscapeCsvField(dt.ToString("yyyy-MM-dd HH:mm:ss"));
+
+        if (value is bool b)
+            return b ? "true" : "false";
+
+        if (value is decimal or double or float)
+            return value.ToString() ?? string.Empty;
+
+        return EscapeCsvField(value.ToString() ?? string.Empty);
+    }
+
+    /// <summary>
+    /// Escapes CSV field by quoting if it contains special characters.
+    /// </summary>
+    private string EscapeCsvField(string field)
+    {
+        if (string.IsNullOrEmpty(field))
+            return string.Empty;
+
+        // Quote field if it contains delimiter, quotes, or newlines
+        if (field.Contains(_delimiter) || field.Contains('"') || field.Contains('\n') || field.Contains('\r'))
+        {
+            return $"\"{field.Replace("\"", "\"\"")}\"";
+        }
+
+        return field;
+    }
+
+    /// <summary>
+    /// Unescapes a quoted CSV field.
+    /// </summary>
+    private string UnescapeCsvField(string field)
+    {
+        if (field.StartsWith("\"") && field.EndsWith("\""))
+        {
+            return field.Substring(1, field.Length - 2).Replace("\"\"", "\"");
+        }
+
+        return field;
+    }
+
+    /// <summary>
+    /// Combines values into a CSV line using the configured delimiter.
+    /// </summary>
+    private string CombineValues(IEnumerable<string> values)
+    {
+        return string.Join(_delimiter.ToString(), values);
+    }
+}
---
+++ Formatters/TextFormatter.cs
@@ -0,0 +1,294 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text;
+
+namespace CoolifiCli.Formatters;
+
+/// <summary>
+/// Formats data as plain text output with various styling options.
+/// Supports colors, alignment, indentation, and structured text layouts.
+/// </summary>
+public class TextFormatter
+{
+    private readonly StringBuilder _content = new();
+    private int _indentLevel = 0;
+    private const int IndentSize = 2;
+
+    /// <summary>
+    /// Adds a plain text line.
+    /// </summary>
+    public TextFormatter WriteLine(string text = "")
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            _content.AppendLine();
+        }
+        else
+        {
+            _content.AppendLine(GetIndent() + text);
+        }
+
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a colored text line.
+    /// </summary>
+    public TextFormatter WriteLineColored(string text, ConsoleColor color)
+    {
+        var colorCode = GetColorCode(color);
+        _content.AppendLine($"{colorCode}{GetIndent()}{text}\x1b[0m");
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a success message (green).
+    /// </summary>
+    public TextFormatter WriteSuccess(string text)
+    {
+        return WriteLineColored($"✓ {text}", ConsoleColor.Green);
+    }
+
+    /// <summary>
+    /// Adds an error message (red).
+    /// </summary>
+    public TextFormatter WriteError(string text)
+    {
+        return WriteLineColored($"✗ {text}", ConsoleColor.Red);
+    }
+
+    /// <summary>
+    /// Adds a warning message (yellow).
+    /// </summary>
+    public TextFormatter WriteWarning(string text)
+    {
+        return WriteLineColored($"⚠ {text}", ConsoleColor.Yellow);
+    }
+
+    /// <summary>
+    /// Adds an info message (cyan).
+    /// </summary>
+    public TextFormatter WriteInfo(string text)
+    {
+        return WriteLineColored($"ℹ {text}", ConsoleColor.Cyan);
+    }
+
+    /// <summary>
+    /// Adds a header with underline.
+    /// </summary>
+    public TextFormatter WriteHeader(string title)
+    {
+        WriteLine(title);
+        WriteLine(new string('=', title.Length));
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a subheader.
+    /// </summary>
+    public TextFormatter WriteSubheader(string title)
+    {
+        WriteLine(title);
+        WriteLine(new string('-', title.Length));
+        return this;
+    }
+
+    /// <summary>
+    /// Increases indentation level.
+    /// </summary>
+    public TextFormatter Indent()
+    {
+        _indentLevel++;
+        return this;
+    }
+
+    /// <summary>
+    /// Decreases indentation level.
+    /// </summary>
+    public TextFormatter Outdent()
+    {
+        if (_indentLevel > 0)
+            _indentLevel--;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a key-value pair.
+    /// </summary>
+    public TextFormatter WriteKeyValue(string key, object? value)
+    {
+        WriteLine($"{key}: {value ?? "-"}");
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a list item with bullet.
+    /// </summary>
+    public TextFormatter WriteListItem(string item)
+    {
+        WriteLine($"• {item}");
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a numbered list item.
+    /// </summary>
+    public TextFormatter WriteNumberedItem(int number, string item)
+    {
+        WriteLine($"{number}. {item}");
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a code block.
+    /// </summary>
+    public TextFormatter WriteCodeBlock(string code)
+    {
+        WriteLine("┌─────────────────────────────────┐");
+        Indent();
+
+        foreach (var line in code.Split('\n'))
+        {
+            WriteLine(line);
+        }
+
+        Outdent();
+        WriteLine("└─────────────────────────────────┘");
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a separator line.
+    /// </summary>
+    public TextFormatter WriteSeparator()
+    {
+        WriteLine(new string('-', 40));
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a formatted progress bar.
+    /// </summary>
+    public TextFormatter WriteProgressBar(int current, int total, string label = "Progress")
+    {
+        var percent = (double)current / total;
+        var filled = (int)(percent * 20);
+        var bar = new string('█', filled) + new string('░', 20 - filled);
+        WriteLine($"{label}: [{bar}] {percent:P0}");
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a table from columns and rows.
+    /// </summary>
+    public TextFormatter WriteTable(string[] headers, string[][] rows)
+    {
+        if (headers.Length == 0 || rows.Length == 0)
+            return this;
+
+        // Calculate column widths
+        var colWidths = new int[headers.Length];
+        for (int i = 0; i < headers.Length; i++)
+        {
+            colWidths[i] = headers[i].Length;
+        }
+
+        foreach (var row in rows)
+        {
+            for (int i = 0; i < headers.Length && i < row.Length; i++)
+            {
+                colWidths[i] = Math.Max(colWidths[i], row[i].Length);
+            }
+        }
+
+        // Write header
+        var headerLine = GetIndent() + string.Join("  ", headers.Select((h, i) => h.PadRight(colWidths[i])));
+        _content.AppendLine(headerLine);
+
+        // Write separator
+        var separatorLine = GetIndent() + string.Join("  ", colWidths.Select(w => new string('-', w)));
+        _content.AppendLine(separatorLine);
+
+        // Write rows
+        foreach (var row in rows)
+        {
+            var rowLine = GetIndent() + string.Join("  ", row.Select((v, i) =>
+                i < colWidths.Length ? v.PadRight(colWidths[i]) : v));
+            _content.AppendLine(rowLine);
+        }
+
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a panel/box with a message.
+    /// </summary>
+    public TextFormatter WritePanel(string content, string? title = null)
+    {
+        var width = Math.Max(content.Length, title?.Length ?? 0) + 4;
+
+        WriteLine("┌" + new string('─', width - 2) + "┐");
+
+        if (!string.IsNullOrEmpty(title))
+        {
+            WriteLine($"│ {title.PadRight(width - 4)} │");
+            WriteLine("├" + new string('─', width - 2) + "┤");
+        }
+
+        foreach (var line in content.Split('\n'))
+        {
+            WriteLine($"│ {line.PadRight(width - 4)} │");
+        }
+
+        WriteLine("└" + new string('─', width - 2) + "┘");
+
+        return this;
+    }
+
+    /// <summary>
+    /// Clears all content.
+    /// </summary>
+    public void Clear()
+    {
+        _content.Clear();
+        _indentLevel = 0;
+    }
+
+    /// <summary>
+    /// Gets the formatted text content.
+    /// </summary>
+    public override string ToString() => _content.ToString();
+
+    /// <summary>
+    /// Writes the content to console.
+    /// </summary>
+    public void Print()
+    {
+        Console.Write(_content.ToString());
+    }
+
+    /// <summary>
+    /// Gets the current indentation as spaces.
+    /// </summary>
+    private string GetIndent() => new string(' ', _indentLevel * IndentSize);
+
+    /// <summary>
+    /// Gets ANSI color code for a console color.
+    /// </summary>
+    private string GetColorCode(ConsoleColor color) => color switch
+    {
+        ConsoleColor.Red => "\x1b[31m",
+        ConsoleColor.Green => "\x1b[32m",
+        ConsoleColor.Yellow => "\x1b[33m",
+        ConsoleColor.Blue => "\x1b[34m",
+        ConsoleColor.Magenta => "\x1b[35m",
+        ConsoleColor.Cyan => "\x1b[36m",
+        ConsoleColor.White => "\x1b[37m",
+        ConsoleColor.Gray => "\x1b[90m",
+        _ => "\x1b[0m"
+    };
+}
