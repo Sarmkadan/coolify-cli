334704a433f46e52c2e676d47e174b17b6ec00c4
Vladyslav Zaiets <vladyslavz@redrocket.software>
2022-05-31 10:11:33 +0300

    Dedupe some logic in ApplicationRepository

---
+++ Models/DatabaseConfiguration.cs
@@ -0,0 +1,116 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace CoolifiCli.Models;
+
+/// <summary>
+/// Represents a database instance managed by Coolify.
+/// Supports multiple database engines with connection pooling and backup configuration.
+/// </summary>
+public class DatabaseConfiguration
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DatabaseType Type { get; set; }
+    public string Version { get; set; } = string.Empty;
+    public string Host { get; set; } = "localhost";
+    public int Port { get; set; }
+    public string RootUsername { get; set; } = "root";
+    public string RootPassword { get; set; } = string.Empty;
+    public string DefaultDatabase { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public int MaxConnections { get; set; } = 100;
+    public int ConnectionTimeoutSeconds { get; set; } = 30;
+    public bool EnableBackups { get; set; } = true;
+    public int BackupRetentionDays { get; set; } = 30;
+    public string BackupSchedule { get; set; } = "0 2 * * *"; // 2 AM daily
+    public bool IsHealthy { get; set; } = true;
+    public DateTime? LastHealthCheckAt { get; set; }
+    public string EnvironmentId { get; set; } = string.Empty;
+    public List<string> AllowedHostPatterns { get; set; } = new();
+
+    /// <summary>
+    /// Validates database configuration for required fields and valid values.
+    /// </summary>
+    /// <returns>Collection of validation error messages.</returns>
+    public IEnumerable<string> Validate()
+    {
+        var errors = new List<string>();
+
+        if (string.IsNullOrWhiteSpace(Name))
+            errors.Add("Database name is required.");
+
+        if (string.IsNullOrWhiteSpace(Host))
+            errors.Add("Database host is required.");
+
+        if (Port < 1 || Port > 65535)
+            errors.Add($"Invalid port: {Port}. Must be between 1 and 65535.");
+
+        if (string.IsNullOrWhiteSpace(RootUsername))
+            errors.Add("Root username is required.");
+
+        if (string.IsNullOrWhiteSpace(RootPassword) || RootPassword.Length < 8)
+            errors.Add("Root password must be at least 8 characters long.");
+
+        if (MaxConnections < 1 || MaxConnections > 1000)
+            errors.Add("Max connections must be between 1 and 1000.");
+
+        if (ConnectionTimeoutSeconds < 5 || ConnectionTimeoutSeconds > 300)
+            errors.Add("Connection timeout must be between 5 and 300 seconds.");
+
+        if (BackupRetentionDays < 1 || BackupRetentionDays > 365)
+            errors.Add("Backup retention days must be between 1 and 365.");
+
+        return errors;
+    }
+
+    /// <summary>
+    /// Returns the default port for the specified database type.
+    /// </summary>
+    /// <param name="dbType">The database type.</param>
+    /// <returns>Default port number for the database type.</returns>
+    public static int GetDefaultPort(DatabaseType dbType) => dbType switch
+    {
+        DatabaseType.PostgreSQL => 5432,
+        DatabaseType.MySQL => 3306,
+        DatabaseType.MongoDB => 27017,
+        DatabaseType.Redis => 6379,
+        _ => 3306
+    };
+
+    /// <summary>
+    /// Builds a connection string appropriate for the database type.
+    /// </summary>
+    /// <returns>Connection string ready for driver use.</returns>
+    public string BuildConnectionString()
+    {
+        return Type switch
+        {
+            DatabaseType.PostgreSQL => $"Host={Host};Port={Port};Username={RootUsername};Password={RootPassword};Database={DefaultDatabase}",
+            DatabaseType.MySQL => $"server={Host};port={Port};uid={RootUsername};pwd={RootPassword};database={DefaultDatabase}",
+            DatabaseType.MongoDB => $"mongodb://{RootUsername}:{RootPassword}@{Host}:{Port}/{DefaultDatabase}",
+            DatabaseType.Redis => $"{Host}:{Port}",
+            _ => string.Empty
+        };
+    }
+
+    /// <summary>
+    /// Marks the database as healthy after successful health check.
+    /// </summary>
+    public void MarkAsHealthy()
+    {
+        IsHealthy = true;
+        LastHealthCheckAt = DateTime.UtcNow;
+    }
+
+    /// <summary>
+    /// Marks the database as unhealthy after failed health check.
+    /// </summary>
+    public void MarkAsUnhealthy()
+    {
+        IsHealthy = false;
+        LastHealthCheckAt = DateTime.UtcNow;
+    }
+}
---
+++ Models/EnvironmentVariable.cs
@@ -0,0 +1,114 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace CoolifiCli.Models;
+
+/// <summary>
+/// Represents an environment variable for an application or service.
+/// Supports value encryption, scoping to environments, and change tracking.
+/// </summary>
+public class EnvironmentVariable
+{
+    public int Id { get; set; }
+    public string ApplicationId { get; set; } = string.Empty;
+    public string Key { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+    public bool IsSecret { get; set; } = false;
+    public string? Description { get; set; }
+    public string EnvironmentScope { get; set; } = "production";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public string? CreatedBy { get; set; }
+    public string? UpdatedBy { get; set; }
+    public bool IsActive { get; set; } = true;
+
+    /// <summary>
+    /// Validates the environment variable key and value.
+    /// </summary>
+    /// <returns>Collection of validation error messages.</returns>
+    public IEnumerable<string> Validate()
+    {
+        var errors = new List<string>();
+
+        if (string.IsNullOrWhiteSpace(Key))
+            errors.Add("Environment variable key is required.");
+
+        if (!IsValidKeyFormat(Key))
+            errors.Add("Environment variable key must contain only alphanumeric characters and underscores.");
+
+        if (string.IsNullOrEmpty(Value) && !IsSecret)
+            errors.Add("Environment variable value cannot be empty for non-secret variables.");
+
+        if (IsSecret && Value.Length > 0 && Value.Length < 8)
+            errors.Add("Secret values should be at least 8 characters long.");
+
+        if (string.IsNullOrWhiteSpace(ApplicationId))
+            errors.Add("Application ID is required.");
+
+        if (string.IsNullOrWhiteSpace(EnvironmentScope))
+            errors.Add("Environment scope is required.");
+
+        return errors;
+    }
+
+    /// <summary>
+    /// Checks if the environment variable key follows naming conventions.
+    /// </summary>
+    /// <param name="key">The key to validate.</param>
+    /// <returns>True if key format is valid.</returns>
+    private static bool IsValidKeyFormat(string key)
+    {
+        if (string.IsNullOrEmpty(key))
+            return false;
+
+        return key.All(c => char.IsLetterOrDigit(c) || c == '_') && !char.IsDigit(key[0]);
+    }
+
+    /// <summary>
+    /// Gets the display value, masking secrets if requested.
+    /// </summary>
+    /// <param name="maskSecrets">Whether to mask secret values.</param>
+    /// <returns>Display value for the variable.</returns>
+    public string GetDisplayValue(bool maskSecrets = true)
+    {
+        if (!IsSecret || !maskSecrets)
+            return Value;
+
+        return Value.Length > 4 ? $"***{Value.Substring(Value.Length - 4)}" : "***";
+    }
+
+    /// <summary>
+    /// Creates a copy of the environment variable for auditing purposes.
+    /// </summary>
+    /// <returns>A new instance with the same properties.</returns>
+    public EnvironmentVariable Clone()
+    {
+        return new EnvironmentVariable
+        {
+            Id = Id,
+            ApplicationId = ApplicationId,
+            Key = Key,
+            Value = Value,
+            IsSecret = IsSecret,
+            Description = Description,
+            EnvironmentScope = EnvironmentScope,
+            CreatedAt = CreatedAt,
+            UpdatedAt = UpdatedAt,
+            CreatedBy = CreatedBy,
+            UpdatedBy = UpdatedBy,
+            IsActive = IsActive
+        };
+    }
+
+    /// <summary>
+    /// Marks the variable as updated with current timestamp and user.
+    /// </summary>
+    /// <param name="updatedBy">User or system performing the update.</param>
+    public void MarkAsUpdated(string updatedBy)
+    {
+        UpdatedAt = DateTime.UtcNow;
+        UpdatedBy = updatedBy;
+    }
+}
