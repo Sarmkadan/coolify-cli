739791fa4c51ca01574afaf3fcf4c5b6c9d8e77a
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-03 14:31:12 +0300

    DateTimeExtensions extensions

--- tests/coolify-cli.Tests/DateTimeExtensionsTests.cs
+++ tests/coolify-cli.Tests/DateTimeExtensionsTests.cs
@@ -6,10 +6,17 @@ using Xunit;
 
 namespace CoolifyCli.Tests;
 
+/// <summary>
+/// Provides unit tests for the DateTime extension methods in the CoolifyCli.Extensions namespace.
+/// Tests cover relative time formatting, duration formatting, date manipulation, and business day calculations.
+/// </summary>
 public class DateTimeExtensionsTests
 {
     // ---- ToRelativeTime ------------------------------------------------------
 
+    /// <summary>
+    /// Tests that a DateTime within the last 500 milliseconds returns "just now".
+    /// </summary>
     [Fact]
     public void ToRelativeTime_WhenJustNow_ReturnsJustNow()
     {
@@ -18,6 +25,9 @@ public class DateTimeExtensionsTests
         result.Should().Be("just now");
     }
 
+    /// <summary>
+    /// Tests that a DateTime approximately 30 seconds in the past returns "30 seconds ago".
+    /// </summary>
     [Fact]
     public void ToRelativeTime_WhenFewSecondsAgo_ReturnsSecondsAgo()
     {
@@ -26,6 +36,9 @@ public class DateTimeExtensionsTests
         result.Should().Be("30 seconds ago");
     }
 
+    /// <summary>
+    /// Tests that a DateTime approximately 1 minute in the past returns a string starting with "1 minute" and ending with "ago".
+    /// </summary>
     [Fact]
     public void ToRelativeTime_WhenAboutOneMinuteAgo_ReturnsMinuteAgo()
     {
@@ -35,6 +48,9 @@ public class DateTimeExtensionsTests
         result.Should().EndWith("ago");
     }
 
+    /// <summary>
+    /// Tests that a DateTime approximately 3 hours in the past returns "3 hours ago".
+    /// </summary>
     [Fact]
     public void ToRelativeTime_WhenSeveralHoursAgo_ReturnsHoursAgo()
     {
@@ -43,6 +59,9 @@ public class DateTimeExtensionsTests
         result.Should().Be("3 hours ago");
     }
 
+    /// <summary>
+    /// Tests that a DateTime approximately 5 days in the past returns "5 days ago".
+    /// </summary>
     [Fact]
     public void ToRelativeTime_WhenSeveralDaysAgo_ReturnsDaysAgo()
     {
@@ -51,6 +70,9 @@ public class DateTimeExtensionsTests
         result.Should().Be("5 days ago");
     }
 
+    /// <summary>
+    /// Tests that a DateTime approximately 2 weeks in the past returns "2 weeks ago".
+    /// </summary>
     [Fact]
     public void ToRelativeTime_WhenTwoWeeksAgo_ReturnsWeeksAgo()
     {
@@ -59,6 +81,9 @@ public class DateTimeExtensionsTests
         result.Should().Be("2 weeks ago");
     }
 
+    /// <summary>
+    /// Tests that a DateTime approximately 3 months in the past returns "3 months ago".
+    /// </summary>
     [Fact]
     public void ToRelativeTime_WhenSeveralMonthsAgo_ReturnsMonthsAgo()
     {
@@ -67,6 +92,9 @@ public class DateTimeExtensionsTests
         result.Should().Be("3 months ago");
     }
 
+    /// <summary>
+    /// Tests that a DateTime approximately 2 years in the past returns "2 years ago".
+    /// </summary>
     [Fact]
     public void ToRelativeTime_WhenOverAYearAgo_ReturnsYearsAgo()
     {
@@ -77,6 +105,9 @@ public class DateTimeExtensionsTests
 
     // ---- ToReadableDuration --------------------------------------------------
 
+    /// <summary>
+    /// Tests that a TimeSpan with only seconds component returns a string with just the seconds value followed by 's'.
+    /// </summary>
     [Fact]
     public void ToReadableDuration_WithOnlySeconds_ShowsSecondsComponent()
     {
@@ -85,6 +116,9 @@ public class DateTimeExtensionsTests
         ts.ToReadableDuration().Should().Be("45s");
     }
 
+    /// <summary>
+    /// Tests that a TimeSpan with hours, minutes, and seconds components returns a formatted string with all components.
+    /// </summary>
     [Fact]
     public void ToReadableDuration_WithHoursMinutesSeconds_ShowsAllComponents()
     {
@@ -93,6 +127,9 @@ public class DateTimeExtensionsTests
         ts.ToReadableDuration().Should().Be("2h 30m 15s");
     }
 
+    /// <summary>
+    /// Tests that a TimeSpan with days and hours components returns a formatted string including the day component.
+    /// </summary>
     [Fact]
     public void ToReadableDuration_WithDays_IncludesDayComponent()
     {
@@ -101,6 +138,9 @@ public class DateTimeExtensionsTests
         ts.ToReadableDuration().Should().Be("3d 1h");
     }
 
+    /// <summary>
+    /// Tests that a zero TimeSpan returns "0s" as its readable duration representation.
+    /// </summary>
     [Fact]
     public void ToReadableDuration_WithZeroTimeSpan_ShowsZeroSeconds()
     {
@@ -111,6 +151,9 @@ public class DateTimeExtensionsTests
 
     // ---- ToFormattedDuration -------------------------------------------------
 
+    /// <summary>
+    /// Tests that a TimeSpan in the sub-second range returns a string showing milliseconds.
+    /// </summary>
     [Fact]
     public void ToFormattedDuration_SubSecondRange_ShowsMilliseconds()
     {
@@ -119,6 +162,9 @@ public class DateTimeExtensionsTests
         ts.ToFormattedDuration().Should().Be("500ms");
     }
 
+    /// <summary>
+    /// Tests that a TimeSpan in the second range returns a string showing seconds with two decimal places.
+    /// </summary>
     [Fact]
     public void ToFormattedDuration_SecondRange_ShowsSeconds()
     {
@@ -127,6 +173,9 @@ public class DateTimeExtensionsTests
         ts.ToFormattedDuration().Should().Be("10.00s");
     }
 
+    /// <summary>
+    /// Tests that a TimeSpan in the minute range returns a string showing minutes with two decimal places.
+    /// </summary>
     [Fact]
     public void ToFormattedDuration_MinuteRange_ShowsMinutes()
     {
@@ -137,6 +186,9 @@ public class DateTimeExtensionsTests
 
     // ---- StartOfDay / EndOfDay -----------------------------------------------
 
+    /// <summary>
+    /// Tests that StartOfDay() returns a DateTime with the same date but time set to midnight (00:00:00).
+    /// </summary>
     [Fact]
     public void StartOfDay_ReturnsDateWithZeroTime()
     {
@@ -147,6 +199,9 @@ public class DateTimeExtensionsTests
         start.Should().Be(new DateTime(2024, 6, 15, 0, 0, 0));
     }
 
+    /// <summary>
+    /// Tests that EndOfDay() returns a DateTime with the same date but time set to just before midnight (23:59:59.9999999).
+    /// </summary>
     [Fact]
     public void EndOfDay_ReturnsLastTickOfDay()
     {
@@ -160,6 +215,9 @@ public class DateTimeExtensionsTests
 
     // ---- StartOfWeek ---------------------------------------------------------
 
+    /// <summary>
+    /// Tests that StartOfWeek() with DayOfWeek.Monday returns the preceding Monday for a given Wednesday.
+    /// </summary>
     [Fact]
     public void StartOfWeek_WithMondayStart_ReturnsPrecedingMonday()
     {
@@ -173,6 +231,9 @@ public class DateTimeExtensionsTests
 
     // ---- StartOfMonth / EndOfMonth -------------------------------------------
 
+    /// <summary>
+    /// Tests that StartOfMonth() returns the first day of the month (day 1) for any DateTime in that month.
+    /// </summary>
     [Fact]
     public void StartOfMonth_ReturnsFirstDayOfMonth()
     {
@@ -181,6 +242,9 @@ public class DateTimeExtensionsTests
         dt.StartOfMonth().Should().Be(new DateTime(2024, 3, 1));
     }
 
+    /// <summary>
+    /// Tests that EndOfMonth() returns the last day of the month, correctly handling month boundaries including leap years.
+    /// </summary>
     [Fact]
     public void EndOfMonth_ReturnsLastDayOfMonth()
     {
@@ -194,12 +258,18 @@ public class DateTimeExtensionsTests
 
     // ---- IsPast / IsFuture ---------------------------------------------------
 
+    /// <summary>
+    /// Tests that IsPast() returns true for a DateTime that is in the past.
+    /// </summary>
     [Fact]
     public void IsPast_ForPastDateTime_ReturnsTrue()
     {
         DateTime.UtcNow.AddDays(-1).IsPast().Should().BeTrue();
     }
 
+    /// <summary>
+    /// Tests that IsFuture() returns true for a DateTime that is in the future.
+    /// </summary>
     [Fact]
     public void IsFuture_ForFutureDateTime_ReturnsTrue()
     {
@@ -208,6 +278,9 @@ public class DateTimeExtensionsTests
 
     // ---- ToIso8601String -----------------------------------------------------
 
+    /// <summary>
+    /// Tests that ToIso8601String() produces a string that can be parsed back to the original DateTime using roundtrip formatting.
+    /// </summary>
     [Fact]
     public void ToIso8601String_ProducesRoundtrippableString()
     {
@@ -221,6 +294,9 @@ public class DateTimeExtensionsTests
 
     // ---- Unix timestamp ------------------------------------------------------
 
+    /// <summary>
+    /// Tests that ToUnixTimestamp() and FromUnixTimestamp() can roundtrip a DateTime through Unix timestamp format.
+    /// </summary>
     [Fact]
     public void ToUnixTimestamp_AndBack_RoundtripsCorrectly()
     {
@@ -234,6 +310,9 @@ public class DateTimeExtensionsTests
 
     // ---- BusinessDaysBetween -------------------------------------------------
 
+    /// <summary>
+    /// Tests that BusinessDaysBetween() counts only weekdays (Monday-Friday) when the date range spans a full workweek.
+    /// </summary>
     [Fact]
     public void BusinessDaysBetween_WorkweekOnly_CountsFiveBusinessDays()
     {
@@ -245,6 +324,9 @@ public class DateTimeExtensionsTests
         count.Should().Be(5);
     }
 
+    /// <summary>
+    /// Tests that BusinessDaysBetween() excludes weekends (Saturday and Sunday) when the date range spans a weekend.
+    /// </summary>
     [Fact]
     public void BusinessDaysBetween_SpanningWeekend_ExcludesSaturdayAndSunday()
     {
@@ -258,6 +340,9 @@ public class DateTimeExtensionsTests
 
     // ---- RoundToMinute -------------------------------------------------------
 
+    /// <summary>
+    /// Tests that RoundToMinute() truncates seconds and sub-seconds, rounding down to the nearest minute.
+    /// </summary>
     [Fact]
     public void RoundToMinute_TruncatesSecondsAndSubSeconds()
     {
