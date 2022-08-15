ff7dfcda8090cb4937ee21d836246beb0ae953b6
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-15 14:55:51 +0300

    Readability pass over DatabaseConfigurationTests
    also touched a couple of call sites.

--- tests/coolify-cli.Tests/DatabaseConfigurationTests.cs
+++ tests/coolify-cli.Tests/DatabaseConfigurationTests.cs
@@ -4,8 +4,14 @@ using CoolifyCli.Models;
 
 namespace CoolifyCli.Tests;
 
+/// <summary>
+/// Tests for the DatabaseConfiguration model validation.
+/// </summary>
 public class DatabaseConfigurationTests
 {
+    /// <summary>
+    /// Validates that an error is returned when the database name is missing.
+    /// </summary>
     [Fact]
     public void Validate_ShouldReturnError_WhenNameIsMissing()
     {
@@ -14,6 +20,9 @@ public class DatabaseConfigurationTests
         errors.Should().Contain("Database name is required.");
     }
 
+    /// <summary>
+    /// Validates that an error is returned when the port number is invalid.
+    /// </summary>
     [Fact]
     public void Validate_ShouldReturnError_WhenPortIsInvalid()
     {
@@ -22,6 +31,9 @@ public class DatabaseConfigurationTests
         errors.Should().Contain("Invalid port: 0. Must be between 1 and 65535.");
     }
 
+    /// <summary>
+    /// Validates that an error is returned when the root password is too short.
+    /// </summary>
     [Fact]
     public void Validate_ShouldReturnError_WhenPasswordIsTooShort()
     {
