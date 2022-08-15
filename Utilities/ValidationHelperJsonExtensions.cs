ea9df8d4ff6c3cd37af3cc06fe46b81ea98ea1fb
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-15 11:46:16 +0300

    feat: json round-trip support for ValidationHelperJson

---
+++ Utilities/ValidationHelperJsonExtensions.cs
@@ -0,0 +1,62 @@
+#nullable enable
+
+using System.Text.Json;
+
+namespace CoolifyCli.Utilities;
+
+/// <summary>
+/// Provides System.Text.Json deserialization extensions for JSON strings.
+/// </summary>
+public static class ValidationHelperJsonExtensions
+{
+    /// <summary>
+    /// JSON serialization options with camelCase naming policy.
+    /// </summary>
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Deserializes a JSON string to an object.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>An object with deserialized data, or null if the JSON is null or empty.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is malformed or cannot be deserialized.</exception>
+    public static object? FromJson(string json)
+    {
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<object>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to an object.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized object if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out object? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<object>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
