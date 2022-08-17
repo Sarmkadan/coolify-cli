3a6ef941d7fcf1e5582e7594f5c8c87bd9409591
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-17 06:20:37 +0300

    handy extensions for ApplicationDeployment

---
+++ Models/ApplicationDeploymentExtensions.cs
@@ -0,0 +1,51 @@
+namespace CoolifyCli.Models;
+
+/// <summary>
+/// Provides extension methods for <see cref="ApplicationDeployment"/>.
+/// </summary>
+public static class ApplicationDeploymentExtensions
+{
+    /// <summary>
+    /// Determines whether the application deployment is active and has a valid health check URL.
+    /// </summary>
+    /// <param name="deployment">The application deployment to check.</param>
+    /// <returns><c>true</c> if the deployment is active and has a valid health check URL; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="deployment"/> is <c>null</c>.</exception>
+    public static bool IsHealthy(this ApplicationDeployment deployment)
+    {
+        ArgumentNullException.ThrowIfNull(deployment);
+
+        return deployment.IsActive && !string.IsNullOrEmpty(deployment.HealthCheckUrl);
+    }
+
+    /// <summary>
+    /// Gets a human-readable string representation of the deployment's status.
+    /// </summary>
+    /// <param name="deployment">The application deployment to get the status string for.</param>
+    /// <returns>A human-readable string representation of the deployment's status.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="deployment"/> is <c>null</c>.</exception>
+    public static string GetStatusString(this ApplicationDeployment deployment)
+    {
+        ArgumentNullException.ThrowIfNull(deployment);
+
+        return deployment.Status switch
+        {
+            DeploymentStatus.Deployed => "Deployed",
+            DeploymentStatus.Failed => $"Failed ({deployment.FailureCount} failures)",
+            _ => deployment.Status.ToString(),
+        };
+    }
+
+    /// <summary>
+    /// Gets the environment variables for the deployment as a read-only dictionary.
+    /// </summary>
+    /// <param name="deployment">The application deployment to get the environment variables for.</param>
+    /// <returns>A read-only dictionary of environment variables.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="deployment"/> is <c>null</c>.</exception>
+    public static IReadOnlyDictionary<string, string> GetEnvironmentVariables(this ApplicationDeployment deployment)
+    {
+        ArgumentNullException.ThrowIfNull(deployment);
+
+        return deployment.EnvironmentVariables;
+    }
+}
