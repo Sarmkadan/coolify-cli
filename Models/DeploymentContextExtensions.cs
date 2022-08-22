26aa0f3a502d8a2a9cec513c1975d0e6dcb86007
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-22 16:07:18 +0300

    feat: add extenison helpers for DeploymentContext
    mostly DeploymentContextExtensions.cs, nothing exciting.

---
+++ Models/DeploymentContextExtensions.cs
@@ -0,0 +1,48 @@
+namespace CoolifyCli.Models;
+
+/// <summary>
+/// Extension methods for <see cref="DeploymentContext"/>.
+/// </summary>
+public static class DeploymentContextExtensions
+{
+    /// <summary>
+    /// Checks if the deployment has completed.
+    /// </summary>
+    /// <param name="context">The deployment context.</param>
+    /// <returns>True if the deployment has completed; otherwise, false.</returns>
+    public static bool HasCompleted(this DeploymentContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        return context.CompletedAt.HasValue;
+    }
+
+    /// <summary>
+    /// Gets the duration of the deployment in a human-readable format.
+    /// </summary>
+    /// <param name="context">The deployment context.</param>
+    /// <returns>A string representing the duration of the deployment.</returns>
+    public static string GetDurationString(this DeploymentContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        var duration = context.GetDuration();
+        var hours = duration.Hours;
+        var minutes = duration.Minutes;
+        var seconds = duration.Seconds;
+
+        return $"{hours}h {minutes}m {seconds}s";
+    }
+
+    /// <summary>
+    /// Checks if the deployment requires approval and has not been approved yet.
+    /// </summary>
+    /// <param name="context">The deployment context.</param>
+    /// <returns>True if the deployment requires approval and has not been approved yet; otherwise, false.</returns>
+    public static bool IsPendingApproval(this DeploymentContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        return context.RequiresApproval && string.IsNullOrEmpty(context.ApprovedBy);
+    }
+}
