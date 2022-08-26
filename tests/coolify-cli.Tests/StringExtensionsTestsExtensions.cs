cd7e9efce315f4dbe2e425746b2b420770b92ff4
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-26 13:14:55 +0300

    feat: more StringExtensions extension methods

--- tests/coolify-cli.Tests/StringExtensionsTestsExtensions.cs
+++ tests/coolify-cli.Tests/StringExtensionsTestsExtensions.cs
@@ -7,12 +7,14 @@ namespace CoolifyCli.Tests;
 
 /// <summary>
 /// Extension methods for <see cref="StringExtensionsTests"/> to facilitate testing scenarios.
+/// Provides assertion helpers for StringExtensions extension methods.
 /// </summary>
 public static class StringExtensionsTestsExtensions
 {
     /// <summary>
     /// Creates a test instance of StringExtensionsTests for use in test scenarios.
     /// </summary>
+    /// <param name="_">The test instance (discard parameter).</param>
     /// <returns>A new instance of StringExtensionsTests.</returns>
     public static StringExtensionsTests CreateTestInstance(this StringExtensionsTests _) => new();
 
@@ -22,11 +24,15 @@ public static class StringExtensionsTestsExtensions
     /// <param name="test">The test instance.</param>
     /// <param name="input">The input string to test.</param>
     /// <param name="expected">The expected PascalCase result.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="input"/> or <paramref name="expected"/> is <see langword="null"/>.</exception>
     public static void AssertToPascalCase(
         this StringExtensionsTests test,
         string input,
         string expected)
     {
+        ArgumentNullException.ThrowIfNull(input);
+        ArgumentNullException.ThrowIfNull(expected);
+
         var result = input.ToPascalCase();
         Assert.Equal(expected, result);
     }
@@ -38,12 +44,17 @@ public static class StringExtensionsTestsExtensions
     /// <param name="input">The input string to test.</param>
     /// <param name="maxLength">The maximum allowed length.</param>
     /// <param name="expected">The expected truncated result.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="input"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="maxLength"/> is negative.</exception>
     public static void AssertTruncate(
         this StringExtensionsTests test,
         string input,
         int maxLength,
         string expected)
     {
+        ArgumentNullException.ThrowIfNull(input);
+        ArgumentOutOfRangeException.ThrowIfNegative(maxLength);
+
         var result = input.Truncate(maxLength);
         Assert.Equal(expected, result);
     }
@@ -55,12 +66,18 @@ public static class StringExtensionsTestsExtensions
     /// <param name="input">The input string to mask.</param>
     /// <param name="showChars">Number of characters to show at start and end.</param>
     /// <param name="expected">The expected masked result.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="input"/> or <paramref name="expected"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="showChars"/> is negative.</exception>
     public static void AssertMaskSensitive(
         this StringExtensionsTests test,
         string input,
         int showChars,
         string expected)
     {
+        ArgumentNullException.ThrowIfNull(input);
+        ArgumentNullException.ThrowIfNull(expected);
+        ArgumentOutOfRangeException.ThrowIfNegative(showChars);
+
         var result = input.MaskSensitive(showChars);
         Assert.Equal(expected, result);
     }
@@ -72,12 +89,16 @@ public static class StringExtensionsTestsExtensions
     /// <param name="input">The input string to split.</param>
     /// <param name="separator">The separator character.</param>
     /// <param name="expected">The expected split and trimmed result.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="input"/> or <paramref name="expected"/> is <see langword="null"/>.</exception>
     public static void AssertSplitTrimmed(
         this StringExtensionsTests test,
         string input,
         char separator,
         string[] expected)
     {
+        ArgumentNullException.ThrowIfNull(input);
+        ArgumentNullException.ThrowIfNull(expected);
+
         var result = input.SplitTrimmed(separator);
         Assert.Equal(expected, result);
     }
