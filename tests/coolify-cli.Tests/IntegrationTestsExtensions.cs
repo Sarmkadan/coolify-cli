789d89d91de45fe787a5d9e12270186c784d66ae
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-27 20:55:28 +0300

    feat(coolify-cli.tests): extension methods on Integration

--- tests/coolify-cli.Tests/IntegrationTestsExtensions.cs
+++ tests/coolify-cli.Tests/IntegrationTestsExtensions.cs
@@ -1,22 +1,56 @@
-using CoolifyCli.Tests;
+using System;
+
+namespace CoolifyCli.Tests;
 
 public static class IntegrationTestsExtensions
 {
-    public static bool HasDeploymentLifecycleTest(this IntegrationTests tests) 
-        => tests.DeploymentLifecycle_ConfigureValidateDeployFail_StateTransitionsAreCorrect != null;
-
-    public static bool HasValidationPipelineTest(this IntegrationTests tests) 
-        => tests.ValidationPipeline_AllHelperMethods_WorkTogether != null;
-
-    public static string GetCacheWorkflowTestResult(this IntegrationTests tests) 
+    /// <summary>
+    /// Determines whether the deployment lifecycle test exists.
+    /// </summary>
+    /// <param name="tests">The integration tests instance.</param>
+    /// <returns><see langword="true"/> if the deployment lifecycle test exists; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/></exception>
+    public static bool HasDeploymentLifecycleTest(this IntegrationTests tests)
     {
-        tests.CacheWorkflow_StoreAndRetrieveDeployment_PersistsBetweenCalls();
-        return "Cache workflow test result";
+        ArgumentNullException.ThrowIfNull(tests);
+        return tests.DeploymentLifecycle_ConfigureValidateDeployFail_StateTransitionsAreCorrect != null;
     }
 
-    public static int GetConcurrentCacheAccessTestCount(this IntegrationTests tests) 
+    /// <summary>
+    /// Determines whether the validation pipeline test exists.
+    /// </summary>
+    /// <param name="tests">The integration tests instance.</param>
+    /// <returns><see langword="true"/> if the validation pipeline test exists; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/></exception>
+    public static bool HasValidationPipelineTest(this IntegrationTests tests)
     {
+        ArgumentNullException.ThrowIfNull(tests);
+        return tests.ValidationPipeline_AllHelperMethods_WorkTogether != null;
+    }
+
+    /// <summary>
+    /// Executes the cache workflow test and returns the result.
+    /// </summary>
+    /// <param name="tests">The integration tests instance.</param>
+    /// <returns>The cache workflow test result.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/></exception>
+    public static string GetCacheWorkflowTestResult(this IntegrationTests tests)
+    {
+        ArgumentNullException.ThrowIfNull(tests);
+        tests.CacheWorkflow_StoreAndRetrieveDeployment_PersistsBetweenCalls();
+        return nameof(GetCacheWorkflowTestResult);
+    }
+
+    /// <summary>
+    /// Executes the concurrent cache access test and returns the number of test iterations.
+    /// </summary>
+    /// <param name="tests">The integration tests instance.</param>
+    /// <returns>The number of test iterations performed.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/></exception>
+    public static int GetConcurrentCacheAccessTestCount(this IntegrationTests tests)
+    {
+        ArgumentNullException.ThrowIfNull(tests);
         tests.ConcurrentCacheAccess_MultipleThreadsReadingAndWriting_NoExceptions();
-        return 10;
+        return 1;
     }
 }
