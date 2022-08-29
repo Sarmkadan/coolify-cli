ac574d91f1c37e35146ff3ca0c20d8f5ea98c7ce
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-29 10:51:45 +0300

    more DeploymentDiff extension methods.

--- tests/coolify-cli.Tests/DeploymentDiffTestsExtensions.cs
+++ tests/coolify-cli.Tests/DeploymentDiffTestsExtensions.cs
@@ -1,38 +1,72 @@
 using CoolifyCli.Tests;
 using Xunit;
 
+/// <summary>
+/// Extension methods for <see cref="DeploymentDiffTests"/> that provide test discovery and execution capabilities.
+/// </summary>
 public static class DeploymentDiffTestsExtensions
 {
-    public static bool HasComputeTests(this DeploymentDiffTests tests) 
-    {
-        return tests.GetType().GetMethods().Any(m => 
-            m.Name == "Compute" && 
-            m.GetParameters().Length == 0 && 
-            typeof(void) == m.ReturnType);
-    }
+	/// <summary>
+	/// Determines whether the <see cref="DeploymentDiffTests"/> class contains compute-related test methods.
+	/// </summary>
+	/// <param name="tests">The test instance to check.</param>
+	/// <returns><see langword="true"/> if compute tests are present; otherwise, <see langword="false"/>.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/>.</exception>
+	public static bool HasComputeTests(this DeploymentDiffTests tests)
+	{
+		ArgumentNullException.ThrowIfNull(tests);
 
-    public static bool HasDeploymentDiffEntryTests(this DeploymentDiffTests tests) 
-    {
-        return tests.GetType().GetMethods().Any(m => 
-            m.Name == "DeploymentDiffEntry_HasChange" && 
-            m.GetParameters().Length == 2 && 
-            typeof(bool) == m.ReturnType);
-    }
+		return tests.GetType().GetMethods().Any(m =>
+			m.Name.StartsWith("Compute_") &&
+			m.GetParameters().Length == 0 &&
+			m.ReturnType == typeof(void));
+	}
 
-    public static void RunAllComputeTests(this DeploymentDiffTests tests) 
-    {
-        tests.Compute_WhenBothConfigurationsIdentical_ReportsNoChanges();
-        tests.Compute_WhenBranchChanged_DetectsOneBranchChange();
-        tests.Compute_WhenRepositoryChanged_FlagsHighRisk();
-        tests.Compute_WhenOnlyBuildCommandChanged_IsNotHighRisk();
-        tests.Compute_WhenEnvVarAdded_IncludesEnvVarChange();
-        tests.Compute_WhenEnvVarRemoved_IncludesDeletionChange();
-        tests.Compute_SetsApplicationIdAndName();
-    }
+	/// <summary>
+	/// Determines whether the <see cref="DeploymentDiffTests"/> class contains DeploymentDiffEntry test methods.
+	/// </summary>
+	/// <param name="tests">The test instance to check.</param>
+	/// <returns><see langword="true"/> if DeploymentDiffEntry tests are present; otherwise, <see langword="false"/>.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/>.</exception>
+	public static bool HasDeploymentDiffEntryTests(this DeploymentDiffTests tests)
+	{
+		ArgumentNullException.ThrowIfNull(tests);
 
-    public static void RunAllDeploymentDiffEntryTests(this DeploymentDiffTests tests) 
-    {
-        tests.DeploymentDiffEntry_HasChange_ReturnsFalseForIdenticalValues();
-        tests.DeploymentDiffEntry_HasChange_ReturnsTrueForDifferentValues();
-    }
+		return tests.GetType().GetMethods().Any(m =>
+			m.Name.StartsWith("DeploymentDiffEntry_") &&
+			m.Name.EndsWith("_HasChange") &&
+			m.GetParameters().Length == 2 &&
+			m.ReturnType == typeof(bool));
+	}
+
+	/// <summary>
+	/// Executes all compute-related test methods on the provided test instance.
+	/// </summary>
+	/// <param name="tests">The test instance to execute.</param>
+	/// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/>.</exception>
+	public static void RunAllComputeTests(this DeploymentDiffTests tests)
+	{
+		ArgumentNullException.ThrowIfNull(tests);
+
+		tests.Compute_WhenBothConfigurationsIdentical_ReportsNoChanges();
+		tests.Compute_WhenBranchChanged_DetectsOneBranchChange();
+		tests.Compute_WhenRepositoryChanged_FlagsHighRisk();
+		tests.Compute_WhenOnlyBuildCommandChanged_IsNotHighRisk();
+		tests.Compute_WhenEnvVarAdded_IncludesEnvVarChange();
+		tests.Compute_WhenEnvVarRemoved_IncludesDeletionChange();
+		tests.Compute_SetsApplicationIdAndName();
+	}
+
+	/// <summary>
+	/// Executes all DeploymentDiffEntry test methods on the provided test instance.
+	/// </summary>
+	/// <param name="tests">The test instance to execute.</param>
+	/// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/>.</exception>
+	public static void RunAllDeploymentDiffEntryTests(this DeploymentDiffTests tests)
+	{
+		ArgumentNullException.ThrowIfNull(tests);
+
+		tests.DeploymentDiffEntry_HasChange_ReturnsFalseForIdenticalValues();
+		tests.DeploymentDiffEntry_HasChange_ReturnsTrueForDifferentValues();
+	}
 }
