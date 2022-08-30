9929ea5f4107bfeb52a1e4c7d3ef0739d1afed01
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-30 14:17:02 +0300

    feat(extensions): tidy up DateTimeExtensionsJson json handling

--- Extensions/DateTimeExtensionsJsonExtensions.cs
+++ Extensions/DateTimeExtensionsJsonExtensions.cs
@@ -10,7 +10,8 @@ namespace CoolifyCli.Extensions
     public static class DateTimeExtensionsJsonExtensions
     {
         // Cached serializer options – camelCase naming, can be reused for all calls.
-        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        // Made immutable to ensure thread safety when reused across multiple calls.
+        private static readonly JsonSerializerOptions _options = new()
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             WriteIndented = false
@@ -24,18 +25,26 @@ namespace CoolifyCli.Extensions
         /// <returns>A JSON string representing the <see cref="DateTime"/>.</returns>
         public static string ToJson(this DateTime dateTime, bool indented = false)
         {
-            // Adjust the WriteIndented flag based on the caller's request.
-            _options.WriteIndented = indented;
-            return JsonSerializer.Serialize(dateTime, _options);
+            // Create a new options instance to avoid mutating the shared cached options.
+            var options = new JsonSerializerOptions(_options)
+            {
+                WriteIndented = indented
+            };
+            return JsonSerializer.Serialize(dateTime, options);
         }
 
         /// <summary>
         /// Deserializes a JSON string into a <see cref="DateTime"/> value.
         /// </summary>
         /// <param name="json">The JSON string representing a <see cref="DateTime"/>.</param>
-        /// <returns>The deserialized <see cref="DateTime"/> value, or <c>null</c> if the JSON is <c>null</c> or empty.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
+        /// <exception cref="JsonException"><paramref name="json"/> is not a valid JSON representation of a <see cref="DateTime"/>.</exception>
+        /// <returns>The deserialized <see cref="DateTime"/> value, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
         public static DateTime? FromJson(string json)
         {
+            ArgumentNullException.ThrowIfNull(json);
+
             if (string.IsNullOrWhiteSpace(json))
                 return null;
 
@@ -46,10 +55,13 @@ namespace CoolifyCli.Extensions
         /// Attempts to deserialize a JSON string into a <see cref="DateTime"/> value.
         /// </summary>
         /// <param name="json">The JSON string representing a <see cref="DateTime"/>.</param>
-        /// <param name="value">When this method returns, contains the deserialized <see cref="DateTime"/> if the operation succeeded; otherwise <c>null</c>.</param>
-        /// <returns><c>true</c> if deserialization succeeded; otherwise <c>false</c>.</returns>
+        /// <param name="value">When this method returns, contains the deserialized <see cref="DateTime"/> if the operation succeeded; otherwise <see langword="null"/>.</param>
+        /// <returns><see langword="true"/> if deserialization succeeded; otherwise <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
         public static bool TryFromJson(string json, out DateTime? value)
         {
+            ArgumentNullException.ThrowIfNull(json);
+
             try
             {
                 value = FromJson(json);
