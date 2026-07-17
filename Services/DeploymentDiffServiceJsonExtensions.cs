e49dec4b655c5b53f7abbe7808d85a01133c2ee6
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    readability pass over DeploymentDiffServiceJsonExtensions.

--- Services/DeploymentDiffServiceJsonExtensions.cs
+++ Services/DeploymentDiffServiceJsonExtensions.cs
@@ -11,7 +11,7 @@ namespace CoolifyCli.Services;
 public static class DeploymentDiffServiceJsonExtensions
 {
     // Cached options: camelCase naming policy, ignore null values.
-    private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+    private static readonly JsonSerializerOptions _options = new()
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
@@ -20,63 +20,59 @@ public static class DeploymentDiffServiceJsonExtensions
     /// <summary>
     /// Serializes a <see cref="DeploymentDiffService"/> instance to a JSON string.
     /// </summary>
+    /// <remarks>
+    /// This method serializes metadata about the service rather than the service itself,
+    /// as <see cref="DeploymentDiffService"/> contains dependencies that cannot be meaningfully serialized.
+    /// </remarks>
     /// <param name="value">The deployment diff service to serialize.</param>
     /// <param name="indented">If true, the output JSON will be indented.</param>
-    /// <returns>A JSON representation of the deployment diff service.</returns>
+    /// <returns>A JSON representation containing service metadata.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
     public static string ToJson(this DeploymentDiffService value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
 
-        // DeploymentDiffService itself cannot be meaningfully serialized due to its dependencies.
-        // Return a simple representation indicating the service type.
         var serviceInfo = new
         {
             ServiceType = "DeploymentDiffService",
             SerializedAt = DateTime.UtcNow
         };
 
-        var options = new JsonSerializerOptions(_options)
-        {
-            WriteIndented = indented
-        };
-
-        return JsonSerializer.Serialize(serviceInfo, options);
+        return JsonSerializer.Serialize(
+            serviceInfo,
+            new JsonSerializerOptions(_options) { WriteIndented = indented }
+        );
     }
 
     /// <summary>
     /// Deserializes a JSON string into a <see cref="DeploymentDiffService"/> instance.
     /// </summary>
+    /// <remarks>
+    /// <see cref="DeploymentDiffService"/> cannot be meaningfully deserialized due to constructor injection requirements.
+    /// This method always returns null.
+    /// </remarks>
     /// <param name="json">The JSON string representing a deployment diff service.</param>
-    /// <returns>The deserialized <see cref="DeploymentDiffService"/> instance, or null if the JSON is empty.</returns>
+    /// <returns>Always null, as the service requires constructor injection.</returns>
     /// <exception cref="JsonException">Thrown when the JSON is invalid.</exception>
     public static DeploymentDiffService? FromJson(string json)
     {
-        if (string.IsNullOrWhiteSpace(json))
-            return null;
-
-        // DeploymentDiffService cannot be meaningfully deserialized due to its dependencies.
-        // Return null as the service requires constructor injection.
+        _ = string.IsNullOrWhiteSpace(json);
         return null;
     }
 
     /// <summary>
     /// Attempts to deserialize a JSON string into a <see cref="DeploymentDiffService"/> instance.
     /// </summary>
+    /// <remarks>
+    /// This method always returns false and sets <paramref name="value"/> to null,
+    /// as <see cref="DeploymentDiffService"/> cannot be deserialized.
+    /// </remarks>
     /// <param name="json">The JSON string representing a deployment diff service.</param>
-    /// <param name="value">When this method returns, contains the deserialized <see cref="DeploymentDiffService"/> if the operation succeeded; otherwise, null.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <param name="value">When this method returns, contains null.</param>
+    /// <returns>Always false.</returns>
     public static bool TryFromJson(string json, out DeploymentDiffService? value)
     {
-        try
-        {
-            value = FromJson(json);
-            return true;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
+        value = null;
+        return false;
     }
 }
